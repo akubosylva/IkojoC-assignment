3e0a2106a726249084e545255b031c662b9ea235
akubosylva <akubosylva@gmail.com>
2023-03-04 19:17:29 +0000

    1c

---
+++ 1c.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+	int i = 0, number = 0;
+	int start_num = 0;
+
+	while (i < 5)
+	{
+		printf("Please enter the number\n");
+		scanf("%d", &number);
+		if (number % 2 == 0)
+		{
+			start_num += number;
+			i++;
+		}
+	}
+	printf("The sum of the five even numbers you entered is %d\n", start_num);
+	return (0);
+}	
